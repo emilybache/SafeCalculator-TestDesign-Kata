ef49ab1419bdb1d4ab34a884ab7550e51b0e69f1
emily <emily@bacheconsulting.com>
2023-10-13 15:07:59 +0200

    reset to starting position

--- java/src/test/java/codingdojo/CalculatorTest.java
+++ java/src/test/java/codingdojo/CalculatorTest.java
@@ -8,16 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class CalculatorTest {
   @Test
   void should_not_throw_when_authorized() {
-
-    Authorizer stubAuthorizer = new StubAuthorizer(true);
-    var calculator = new SafeCalculator(stubAuthorizer);
-    assertEquals(3, calculator.add(1, 2));
+    // TODO: write this test
   }
 
-  @Test
-  void should_throw_when_not_authorized() {
-    Authorizer stubAuthorizer = new StubAuthorizer(false);
-    var calculator = new SafeCalculator(stubAuthorizer);
-    assertThrows(UnauthorizedAccessException.class, () -> calculator.add(1,2));
-  }
 }
