4ece93af51dd9811594fce918bbbfee28d667416
Nicolas Barbey <nicolas.barbey@adevinta.com>
2024-05-29 14:28:41 +0200

    go answer: add comment to point to the bug

--- go/safe_calculator.go
+++ go/safe_calculator.go
@@ -7,6 +7,7 @@ type Calculator struct {
 func (c *Calculator) Add(x, y int) int {
 	authorized := c.authorizer.Authorize()
 
+	// BUG! Should be if !authorized
 	if authorized {
 		panic("Not authorized")
 	}
